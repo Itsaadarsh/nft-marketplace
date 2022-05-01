092b03a3453e24e0482d79e2a4da1213fd473308
Itsaadarsh <heyitsaadarsh@gmail.com>
2022-05-01 14:43:30 +0530

    Setting up hardhat development env

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const projectId = fs.readFileSync('.projectId').toString();
+const privateKey = fs.readFileSync('.secret').toString();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
+ 
